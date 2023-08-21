abd9641554d10e0f844a5dcbdeb980fe42cdeeaa
Jake Braun <jakebraun14@gmail.com>
2023-08-21 11:38:56 -0500

    Finished ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let total = 0
+    let arr = [int1, int2]
+    arr.sort(function (a, b) {return a - b})
 
+    if (int1 < 0 | int2 < 0 | Number.isInteger(int1) === false | Number.isInteger(int2) === false) {
+        total = "ERROR"
+    } else {
+        for (i=arr[0]; i < arr[1] + 1; i++) {
+            total += i
+        }
+    }
+    return total
 };
 
 // Do not edit below this line
