6ef55fc7fc30a45fee5800911f64c46fde674340
Jake Braun <jakebraun14@gmail.com>
2023-08-21 11:52:48 -0500

    Finished ex 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  if (Number.isNaN(temp)) {return "ERROR"}
+  return Math.round((temp - 32) * 5 / 9 * 10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  if (Number.isNaN(temp)) {return "ERROR"}
+  return Math.round((temp * 9 / 5 + 32) * 10)/10
 };
 
 // Do not edit below this line
