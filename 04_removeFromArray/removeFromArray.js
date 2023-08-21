c6c2518e97e16ffbec259000f824a4a1f9af7f1a
Jake Braun <jakebraun14@gmail.com>
2023-08-21 11:24:28 -0500

    Finished ex 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    args = Array.from(arguments)
+    arr = args.shift()
+    for (arg of args) {
+        const index = arr.indexOf(arg)
+        if (index > -1) {
+            arr.splice(index, 1)
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
