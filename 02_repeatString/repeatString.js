acb648ad10894747e533854711b168ccdce6d03d
Jake Braun <jakebraun14@gmail.com>
2023-08-21 10:54:57 -0500

    Finished ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, rpt) {
+    let output_string = ""
+    rpt = Number(rpt)
+    if (rpt < 0) {
+        output_string = "ERROR"
+    } else {
+        for (i=0; i<rpt; i++) {
+            output_string = output_string + word
+        }
+    }
+    return output_string
 };
 
 // Do not edit below this line
