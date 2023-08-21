2c4a36e9e897274c75b21d8cc5d59b5fafd33595
Jake Braun <jakebraun14@gmail.com>
2023-08-21 11:45:34 -0500

    Finished ex 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (Number.isInteger(year/100)) {
+        return Number.isInteger(year/400)
+    } else {
+        return Number.isInteger(year/4)
+    }
 };
 
 // Do not edit below this line
