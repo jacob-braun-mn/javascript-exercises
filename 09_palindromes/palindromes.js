4eded4ffb6eb9055842ad83fe6415616fcd1280d
Jake Braun <jakebraun14@gmail.com>
2023-08-24 14:49:29 -0500

    Finished ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.toLowerCase().replace(/(\s)|([^a-z0-9])/g, '');
+    let arr = input.split('');
+    let newString = arr.reverse().join('');
+    return input === newString;
 };
 
 // Do not edit below this line
