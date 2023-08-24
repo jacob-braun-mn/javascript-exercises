aca5e1e3408cb032f772919e95db1b531aa1495c
Jake Braun <jakebraun14@gmail.com>
2023-08-24 14:28:30 -0500

    Finished ex 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  array.forEach(value => sum += value);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  array.forEach(value => product *= value);
+  return product
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let prod = 1;
+  while (a > 0) {
+    prod *= a;
+    a -= 1;
+  }
+  return prod;
 };
 
 // Do not edit below this line
