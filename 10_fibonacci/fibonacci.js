5f22bd3027df4310617c86f4e4d6a3b1805128b7
Jake Braun <jakebraun14@gmail.com>
2023-08-24 14:57:10 -0500

    finished ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    a = Number(a);
+    if (a < 0) {
+        return "OOPS";
+    };
 
+    if (a === 1 | a === 2) {
+        return 1;
+    };
+
+    let fib = [1, 1]
+    for (i=2; i<a; i++) {
+        fib[i] = fib[i-1] + fib[i-2]
+    };
+    return fib[a-1];
 };
 
 // Do not edit below this line
