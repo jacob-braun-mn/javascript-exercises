90fbca85943eb71b279f53b2310679fed55ec929
Jake Braun <jakebraun14@gmail.com>
2023-08-24 15:27:31 -0500

    Finished ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestName;
+    let oldestAge = 0;
+    people.forEach(function(person) {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        };
+        if (person.yearOfDeath - person.yearOfBirth > oldestAge) {
+            oldestAge = person.yearOfDeath - person.yearOfBirth;
+            oldestName = person.name;
+        };
+    });
+    return people.find(person => person.name === oldestName)
 };
 
 // Do not edit below this line
